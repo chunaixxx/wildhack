a489f08d33de0461050fd10f1bbceddfcbe3de6f
VashUber <mrjarokvk@gmail.com>
2021-12-04 13:34:58 +0300

    added classifier

--- utils/getAnswer.js
+++ utils/getAnswer.js
@@ -1,12 +1,16 @@
-const natural = require('natural');
+const natural = require("natural");
 
-const getAnswer = message => {
-    return new Promise((res, rej) => {
-        natural.BayesClassifier.load('classifier.json', null, (err, classifier) => {
-            if (err) rej()
-            res(classifier.getClassifications(message))
-        })
-    })
-}
+const RU = natural.PorterStemmerRu;
 
-module.exports = getAnswer
+const getAnswer = (message) => {
+  return new Promise((res, rej) => {
+    natural.BayesClassifier.load("classifier.json", RU, (err, classifier) => {
+      const values = classifier.getClassifications(message);
+      const answer = values.sort((a, b) => a - b)[0].label;
+      if (err) rej();
+      res(answer);
+    });
+  });
+};
+
+module.exports = getAnswer;
